9d8509ef119ed7221aeaa7c7dcb180847e967524
riekehieke <30436310+riekehieke@users.noreply.github.com>
2020-02-22 16:59:38 +0100

    motor drehen

--- servo.ino
+++ servo.ino
@@ -1,4 +1,4 @@
-#include <Servo.h>  //Die Servobibliothek wird aufgerufen.
+#include <Servo.h> //Die Servobibliothek wird aufgerufen.
 
 Servo servoblau;
 
@@ -6,17 +6,24 @@ int incoming;
 
 // Erstellt für das Programm ein Servo mit dem Namen
 // „servoblau“
-void setup() {
+void setup()
+{
     Serial.begin(9600);
-    servoblau.attach(8);  // Servo wird mit Pin8 verbunden
+    servoblau.attach(8); // Servo wird mit Pin8 verbunden
 }
 
-void loop() {
-    if (Serial.available() > 0) {
+void loop()
+{
+    if (Serial.available() > 0)
+    {
         incoming = Serial.parseInt();
-        if (incoming <= 360 && incoming >= 0 && Serial.read() != -1) {
+        Serial.println(incoming);
+        if (incoming == 5)
+        {
             Serial.println(incoming);
-            servoblau.write(incoming);
+            servoblau.write(170);
+            delay(2000);
+            servoblau.write(90);
         }
     }
     delay(100);
