0304f11925cabb68bdb8ba68da05bf6d61fb4515
riekehieke <30436310+riekehieke@users.noreply.github.com>
2020-02-23 12:15:27 +0100

    led -> kettle

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,19 +1,27 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <Servo.h>
 #include <SocketIoClient.h>
+#include <pins_arduino.h>
 #include "config.h"
 
 SocketIoClient socketClient;
 
-void turnLedOn(const char *payload, size_t length) {
-    digitalWrite(LED_PIN, HIGH);
-    socketClient.emit(TURN_LED_ON_SUCCESS);
+void turnKettleOn(const char *payload, size_t length) {
+    motor.write(90);
+    delay(1000);
+    motor.write(180);
+    //digitalWrite(LED_PIN, HIGH);
+    socketClient.emit(TURN_KETTLE_ON_SUCCESS);
 }
+
+Servo motor;
 
 void setup() {
     Serial.begin(115200);
 
-    pinMode(LED_PIN, OUTPUT);
+    // pinMode(LED_PIN, OUTPUT);
+    motor.attach(SERVO_PIN);
 
     Serial.println("Connecting WiFi...");
     WiFi.mode(WIFI_STA);
@@ -28,7 +36,7 @@ void setup() {
     Serial.println("WiFi connected.");
 
     // Attach socket event handlers
-    socketClient.on(TURN_LED_ON, turnLedOn);
+    socketClient.on(TURN_KETTLE_ON, turnKettleOn);
 
     Serial.println("Connecting to Server...");
     socketClient.begin(SOCKET_SERVER_ADDRESS);
--- server/index.js
+++ server/index.js
@@ -1,47 +1,49 @@
-const path = require('path');
-const http = require('http');
-const express = require('express');
-const socketIo = require('socket.io');
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketIo = require("socket.io");
 const log = console.log;
 
 const PORT = Number(process.env.PORT) || 3030;
 
 const EVENT_TYPES = {
-  TURN_LED_ON: 'TURN_LED_ON',
-  TURN_LED_ON_SUCCESS: 'TURN_LED_ON_SUCCESS',
+  // TURN_LED_ON: 'TURN_LED_ON',
+  // TURN_LED_ON_SUCCESS: 'TURN_LED_ON_SUCCESS',
+  TURN_KETTLE_ON: "TURN_KETTLE_ON",
+  TURN_KETTLE_ON_SUCCESS: "TURN_KETTLE_ON_SUCCESS"
 };
 
 const app = express();
 const server = http.createServer(app);
 
 const io = socketIo(server);
-const smarthubSocket = io.of('/smarthub');
+const smarthubSocket = io.of("/smarthub");
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/index.html"));
 });
 
-app.post('/led', (req, res) => {
-  log(`Received "${EVENT_TYPES.TURN_LED_ON}" from HTTP endpoint!`);
-  io.clients().emit(EVENT_TYPES.TURN_LED_ON);
-  res.send('ok');
+app.post("/led", (req, res) => {
+  log(`Received "${EVENT_TYPES.TURN_KETTLE_ON}" from HTTP endpoint!`);
+  io.clients().emit(EVENT_TYPES.TURN_KETTLE_ON);
+  res.send("ok");
 });
 
-smarthubSocket.on('connection', client => {
+smarthubSocket.on("connection", client => {
   log(`Smarthub client connected! (${client.id})`);
 
-  client.on(EVENT_TYPES.TURN_LED_ON, () => {
-    log(`Received "${EVENT_TYPES.TURN_LED_ON}" from smarthub!`);
-    io.clients().emit(EVENT_TYPES.TURN_LED_ON);
+  client.on(EVENT_TYPES.TURN_KETTLE_ON, () => {
+    log(`Received "${EVENT_TYPES.TURN_KETTLE_ON}" from smarthub!`);
+    io.clients().emit(EVENT_TYPES.TURN_KETTLE_ON);
   });
 });
 
-io.on('connection', client => {
+io.on("connection", client => {
   log(`Generic client connected! (${client.id})`);
 
-  client.on(EVENT_TYPES.TURN_LED_ON_SUCCESS, () => {
-    smarthubSocket.clients().emit(EVENT_TYPES.TURN_LED_ON_SUCCESS);
-    log('The LED is now on!');
+  client.on(EVENT_TYPES.TURN_KETTLE_ON_SUCCESS, () => {
+    smarthubSocket.clients().emit(EVENT_TYPES.TURN_KETTLE_ON_SUCCESS);
+    log("The LED is now on!");
   });
 });
 
