f059326bc6e4ccc04b082fec35ce08501c7e234c
Jonas Kuske <30421456+jonaskuske@users.noreply.github.com>
2020-02-25 02:26:20 +0100

    update socket url for prod

--- controller/config.h
+++ controller/config.h
@@ -7,7 +7,7 @@
 const char *WIFI_SSID = "Internet";
 const char *WIFI_PASSWORD = "Safety1st";
 
-const char *SOCKET_SERVER_ADDRESS = "192.168.0.20";
-const int SOCKET_SERVER_PORT = 3030;
+const char *SOCKET_SERVER_ADDRESS = "smarthub.jonaskuske.de";
+const int SOCKET_SERVER_PORT = 80;
 
 #endif
